89952995b5c81138f2206641530363f01425c035
Yojan21 <yojan.romero.a@gmail.com>
2025-01-11 17:47:34 -0500

    Commit final

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     navegacion_fija();
     crearGaleria();
+    resaltarEnlace();
+    scrollNav();
 });
 
 function navegacion_fija(){
@@ -20,9 +22,12 @@ function crearGaleria(){
     const galeria = document.querySelector('.galeria_imagenes');
     const cantidad_imagenes = 16;
     for (let i = 1; i <= cantidad_imagenes; i++){
-        const imagen = document.createElement('IMG');
-        imagen.src = `src/img/gallery/full/${i}.jpg`;
-        imagen.alt = 'Imagen galeria';
+        const imagen = document.createElement('PICTURE');
+        imagen.innerHTML = `
+        <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
 
         //Event.handler
         imagen.onclick = function(){
@@ -34,9 +39,12 @@ function crearGaleria(){
 
 function mostrarImagen(i){
 
-    const imagen = document.createElement('IMG');
-    imagen.src = `src/img/gallery/full/${i}.jpg`;
-    imagen.alt = 'Imagen galeria';
+    const imagen = document.createElement('PICTURE');
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+`;
 
     //Generar modal
     const modal = document.createElement('DIV');
@@ -68,4 +76,40 @@ function cerrarModal(){
     setTimeout(() => {
         modal?.remove();
     }, 500);
+}
+
+function resaltarEnlace(){
+    document.addEventListener('scroll', function(){
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.navegacion_principal a');
+        let actual = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if(window.scrollY >= (sectionTop - sectionHeight / 3)){
+                actual = section.id;
+            }
+        });
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if(link.getAttribute('href') === '#' + actual){
+                link.classList.add('active');
+            }
+        });
+    });
+}
+
+function scrollNav(){
+    const navLinks = document.querySelectorAll('.navegacion_principal a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', e =>{
+            e.preventDefault();
+            const sectionScroll = e.target.getAttribute('href');
+            const section = document.querySelector(sectionScroll);
+
+            section.scrollIntoView({behavior: 'smooth'});
+        });
+    });
 }
